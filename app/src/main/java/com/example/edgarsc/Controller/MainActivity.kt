9a2e75e341485e16a2bb2c1694f8e53cf3173d0b
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-06 17:20:14 +0200

    Lesson 02

--- app/src/main/java/com/example/edgarsc/Controller/MainActivity.kt
+++ app/src/main/java/com/example/edgarsc/Controller/MainActivity.kt
@@ -2,12 +2,25 @@ package com.example.edgarsc.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.edgarsc.Model.Category
 import com.example.edgarsc.R
+import com.example.edgarsc.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //middle man pa vidu list view un datiem
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
