87589e6c29b819ea6c2e1c7b786cdceab6a15dec
Edgars Cimdiņš <edgars.cimdins@tilde.com>
2019-03-07 13:19:38 +0200

    Lesson 03 custom adapter for listview views

--- app/src/main/java/com/example/edgarsc/Controller/MainActivity.kt
+++ app/src/main/java/com/example/edgarsc/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.edgarsc.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.edgarsc.Adapters.CategoryAdapter
 import com.example.edgarsc.Model.Category
 import com.example.edgarsc.R
 import com.example.edgarsc.Services.DataService
@@ -11,15 +12,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     //middle man pa vidu list view un datiem
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
